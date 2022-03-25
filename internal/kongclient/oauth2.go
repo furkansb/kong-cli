d890a55455e6fc03f2fc17b28c8de66ce71db519
Furkan <31251553+furkansb@users.noreply.github.com>
2022-03-25 18:40:51 +0300

    fix: better error handling for rest of the kong-client (#4)

--- internal/kongclient/oauth2.go
+++ internal/kongclient/oauth2.go
@@ -7,68 +7,66 @@ import (
 	"github.com/kong/go-kong/kong"
 )
 
-func (k *KongManager) CreateOauth2Credential(ctx context.Context, consumerUsernameOrID string, credential *kong.Oauth2Credential) *kong.Oauth2Credential {
+func (k *KongManager) CreateOauth2Credential(ctx context.Context, consumerUsernameOrID string, credential *kong.Oauth2Credential) (*kong.Oauth2Credential, error) {
 	schema, err := k.client.Schemas.Get(ctx, "oauth2_credentials")
 	if err != nil {
-		fmt.Printf("Error getting schema: %s", err)
-		return nil
+		return nil, fmt.Errorf("error getting schema: %s", err)
 	}
 	err = kong.FillEntityDefaults(credential, schema)
 	if err != nil {
-		fmt.Printf("Error filling entity defaults: %s", err)
-		return nil
+		return nil, fmt.Errorf("error filling entity defaults: %s", err)
 	}
 	credential, err = k.client.Oauth2Credentials.Create(ctx, &consumerUsernameOrID, credential)
 	if err != nil {
-		fmt.Printf("Error creating credential: %s", err)
-		return nil
+		return nil, fmt.Errorf("error creating credential: %s", err)
 	}
-	return credential
+	return credential, nil
 }
 
-func (k *KongManager) DeleteOauth2Credential(ctx context.Context, consumerUsernameOrID string, clientIDorID string) {
+func (k *KongManager) DeleteOauth2Credential(ctx context.Context, consumerUsernameOrID string, clientIDorID string) error {
 	err := k.client.Oauth2Credentials.Delete(ctx, &consumerUsernameOrID, &clientIDorID)
 	if err != nil {
-		fmt.Printf("Error deleting credential: %s", err)
+		return fmt.Errorf("error deleting credential: %s", err)
 	}
+	return nil
 }
 
-func (k *KongManager) UpdateOauth2Credential(ctx context.Context, consumerUsernameOrID string, credential *kong.Oauth2Credential) *kong.Oauth2Credential {
+func (k *KongManager) UpdateOauth2Credential(ctx context.Context, consumerUsernameOrID string, credential *kong.Oauth2Credential) (*kong.Oauth2Credential, error) {
 	credential, err := k.client.Oauth2Credentials.Update(ctx, &consumerUsernameOrID, credential)
 	if err != nil {
-		fmt.Printf("Error updating credential: %s", err)
-		return nil
+		return nil, fmt.Errorf("error updating credential: %s", err)
 	}
-	return credential
+	return credential, nil
 }
 
-func (k *KongManager) GetOauth2Credential(ctx context.Context, consumerUsernameOrID string, clientIDorID string) *kong.Oauth2Credential {
+func (k *KongManager) GetOauth2Credential(ctx context.Context, consumerUsernameOrID string, clientIDorID string) (*kong.Oauth2Credential, error) {
 	credential, err := k.client.Oauth2Credentials.Get(ctx, &consumerUsernameOrID, &clientIDorID)
 	if err != nil {
-		fmt.Printf("Error getting credential: %s", err)
-		return nil
+		return nil, fmt.Errorf("error getting credential: %s", err)
 	}
-	return credential
+	return credential, nil
 }
 
-func (k *KongManager) ListAllOauth2Credentials(ctx context.Context, consumerUsernameOrID string) []*kong.Oauth2Credential {
+func (k *KongManager) ListAllOauth2Credentials(ctx context.Context, consumerUsernameOrID string) ([]*kong.Oauth2Credential, error) {
 	credentials, err := k.client.Oauth2Credentials.ListAll(ctx)
 	if err != nil {
-		fmt.Printf("Error listing credentials: %s", err)
-		return nil
+		return nil, fmt.Errorf("error listing credentials: %s", err)
 	}
-	return credentials
+	return credentials, nil
 }
 
-func (k *KongManager) ListAllOauth2CredentialsForConsumer(ctx context.Context, consumerUsernameOrID string) []*kong.Oauth2Credential {
+func (k *KongManager) ListAllOauth2CredentialsForConsumer(ctx context.Context, consumerUsernameOrID string) ([]*kong.Oauth2Credential, error) {
 	credentials, _, err := k.client.Oauth2Credentials.ListForConsumer(ctx, &consumerUsernameOrID, &kong.ListOpt{})
 	if err != nil {
-		fmt.Printf("Error listing credentials: %s", err)
-		return nil
+		return nil, fmt.Errorf("error listing credentials: %s", err)
 	}
-	return credentials
+	return credentials, nil
 }
 
-func Oauth2String(oauth2Credential kong.Oauth2Credential) string {
-	return fmt.Sprintf("Oauth2 ClientID: %s, Consumer: %s\n", *oauth2Credential.ClientID, *oauth2Credential.Consumer.Username)
+func Oauth2String(oauth2Credential *kong.Oauth2Credential) (string, error) {
+	if oauth2Credential == nil {
+		return "", nil
+	}
+	
+	return fmt.Sprintf("Oauth2 ClientID: %s, Consumer: %s\n", strPointerToStr(oauth2Credential.ClientID), strPointerToStr(oauth2Credential.Consumer.Username)), nil
 }
--- internal/kongclient/plugins.go
+++ internal/kongclient/plugins.go
@@ -7,86 +7,81 @@ import (
 	"github.com/kong/go-kong/kong"
 )
 
-func (k *KongManager) CreatePlugin(ctx context.Context, plugin *kong.Plugin) *kong.Plugin {
+func (k *KongManager) CreatePlugin(ctx context.Context, plugin *kong.Plugin) (*kong.Plugin, error) {
 	schema, err := k.client.Schemas.Get(ctx, "plugins")
 	if err != nil {
-		fmt.Printf("Error getting schema: %s", err)
-		return nil
+		return nil, fmt.Errorf("error getting schema: %s", err)
 	}
 	err = kong.FillEntityDefaults(plugin, schema)
 	if err != nil {
-		fmt.Printf("Error filling entity defaults: %s", err)
-		return nil
+		return nil, fmt.Errorf("error filling entity defaults: %s", err)
 	}
 	plugin, err = k.client.Plugins.Create(ctx, plugin)
 	if err != nil {
-		fmt.Printf("Error creating plugin: %s", err)
-		return nil
+		return nil, fmt.Errorf("error creating plugin: %s", err)
 	}
-	return plugin
+	return plugin, nil
 }
 
-func (k *KongManager) DeletePlugin(ctx context.Context, usernameOrID string) {
+func (k *KongManager) DeletePlugin(ctx context.Context, usernameOrID string) error {
 	err := k.client.Plugins.Delete(ctx, &usernameOrID)
 	if err != nil {
-		fmt.Printf("Error deleting plugin: %s", err)
+		return fmt.Errorf("error deleting plugin: %s", err)
 	}
+	return nil
 }
 
-func (k *KongManager) UpdatePlugin(ctx context.Context, plugin *kong.Plugin) *kong.Plugin {
+func (k *KongManager) UpdatePlugin(ctx context.Context, plugin *kong.Plugin) (*kong.Plugin, error) {
 	plugin, err := k.client.Plugins.Update(ctx, plugin)
 	if err != nil {
-		fmt.Printf("Error updating plugin: %s", err)
-		return nil
+		return nil, fmt.Errorf("error updating plugin: %s", err)
 	}
-	return plugin
+	return plugin, nil
 }
 
-func (k *KongManager) GetPlugin(ctx context.Context, usernameOrID string) *kong.Plugin {
+func (k *KongManager) GetPlugin(ctx context.Context, usernameOrID string) (*kong.Plugin, error) {
 	plugin, err := k.client.Plugins.Get(ctx, &usernameOrID)
 	if err != nil {
-		fmt.Printf("Error getting plugin: %s", err)
-		return nil
+		return nil, fmt.Errorf("error getting plugin: %s", err)
 	}
-	return plugin
+	return plugin, nil
 }
 
-func (k *KongManager) ListAllPlugins(ctx context.Context) []*kong.Plugin {
+func (k *KongManager) ListAllPlugins(ctx context.Context) ([]*kong.Plugin, error) {
 	plugins, err := k.client.Plugins.ListAll(ctx)
 	if err != nil {
-		fmt.Printf("Error listing plugins: %s", err)
-		return nil
+		return nil, fmt.Errorf("error listing plugins: %s", err)
 	}
-	return plugins
+	return plugins, nil
 }
 
-func (k *KongManager) ListAllPluginsForConsumer(ctx context.Context, consumerIDorName string) []*kong.Plugin {
+func (k *KongManager) ListAllPluginsForConsumer(ctx context.Context, consumerIDorName string) ([]*kong.Plugin, error) {
 	plugins, err := k.client.Plugins.ListAllForConsumer(ctx, &consumerIDorName)
 	if err != nil {
-		fmt.Printf("Error listing plugins: %s", err)
-		return nil
+		return nil, fmt.Errorf("error listing plugins: %s", err)
 	}
-	return plugins
+	return plugins, nil
 }
 
-func (k *KongManager) ListAllPluginsForService(ctx context.Context, serviceIDorName string) []*kong.Plugin {
+func (k *KongManager) ListAllPluginsForService(ctx context.Context, serviceIDorName string) ([]*kong.Plugin, error) {
 	plugins, err := k.client.Plugins.ListAllForService(ctx, &serviceIDorName)
 	if err != nil {
-		fmt.Printf("Error listing plugins for service: %s", err)
-		return nil
+		return nil, fmt.Errorf("error listing plugins for service: %s", err)
 	}
-	return plugins
+	return plugins, nil
 }
 
-func (k *KongManager) ListAllPluginsForRoute(ctx context.Context, routeIDorName string) []*kong.Plugin {
+func (k *KongManager) ListAllPluginsForRoute(ctx context.Context, routeIDorName string) ([]*kong.Plugin, error) {
 	plugins, err := k.client.Plugins.ListAllForRoute(ctx, &routeIDorName)
 	if err != nil {
-		fmt.Printf("Error listing plugins for route: %s", err)
-		return nil
+		return nil, fmt.Errorf("error listing plugins for route: %s", err)
 	}
-	return plugins
+	return plugins, nil
 }
 
-func PluginString(plugin *kong.Plugin) string {
-	return fmt.Sprintf("Plugin ID: %s, Name: %s\n", *plugin.ID, *plugin.Name)
+func PluginString(plugin *kong.Plugin) (string, error) {
+	if plugin == nil {
+		return "", fmt.Errorf("plugin is nil")
+	}
+	return fmt.Sprintf("Plugin ID: %s, Name: %s\n", *plugin.ID, *plugin.Name), nil
 }
